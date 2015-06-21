db27be27770f035b40b38202641e8e34fa4b2541
Anonymus <Geowc10@gmail.com>
2015-06-21 16:19:02 -0400

    Create PasswordGen.js

---
+++ PasswordGen.js
@@ -0,0 +1,18 @@
+var passwordA = Math.floor((Math.random() * 6) + 1);
+var passwordB = Math.floor((Math.random() * 6) + 1);
+var passwordC = Math.floor((Math.random() * 6) + 1);
+var passwordD = Math.floor((Math.random() * 6) + 1);
+var passwordE = Math.floor((Math.random() * 6) + 1);
+var passwordF = Math.floor((Math.random() * 6) + 1);
+
+var passwordLG = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var passwordL = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+var passwordL1 = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+var passwordL2 = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+var passwordL3 = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+var passwordL4 = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+var passwordL5 = passwordLG[Math.floor(Math.random() * passwordLG.length)];
+
+alert(passwordA +""+ passwordB +""+ passwordL1 +""+passwordL2 +""+ passwordC +""+ passwordD +""+ passwordE +""+ passwordF + ""+ passwordL + "" + passwordL3 +""+ passwordL4 +""+ passwordF +""+ passwordL5);
+//
